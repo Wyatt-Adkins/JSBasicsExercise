828310c112e35e5b44ebfe5db644df4d9a6e542f
wyattadkins@icloud.com <wyattadkins@icloud.com>
2022-09-26 16:25:13 -0500

    first push

---
+++ script.js
@@ -0,0 +1,39 @@
+// 1
+console.log(10);
+
+// 2
+// Numbers,Strings,Booleans,Null,Undefined
+
+// 3
+//  * \ = - % **
+
+// 4
+console.log(5 % 2);
+console.log(5 % 4);
+
+// 5
+console.log('I\'M MARRY POPPINS Y\'ALL!')
+
+// 6
+console.log("Hawk" + "Eye"); 
+
+// 7
+console.log("Wyatt".length + "Adkins".length);
+
+// 8
+console.log("Avengers: Endgame"[16]);
+
+// BONUS
+
+// 1
+console.log("\\");
+
+// 2
+console.log(5 ** 4 - 9);
+
+// 3
+
+console.log("I".length - "Am".length + "Iron".length - "Man".length);
+
+// 4
+// ?????
