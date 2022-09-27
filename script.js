c6e5f440a748883fc00bc0bfd5c67dd31ec59856
wyattadkins@icloud.com <wyattadkins@icloud.com>
2022-09-27 09:52:04 -0500

    finished and corrected

--- script.js
+++ script.js
@@ -2,10 +2,25 @@
 console.log(10);
 
 // 2
-// Numbers,Strings,Booleans,Null,Undefined
+/* 
+Numbers,
+Strings,
+Booleans,
+Null,
+Undefined
+*/
 
 // 3
-//  * \ = - % **
+/* 
+
+* 
+\ 
+= 
+- 
+% 
+** 
+
+*/
 
 // 4
 console.log(5 % 2);
@@ -26,7 +41,7 @@ console.log("Avengers: Endgame"[16]);
 // BONUS
 
 // 1
-console.log("\\");
+console.log("\/");
 
 // 2
 console.log(5 ** 4 - 9);
@@ -36,4 +51,22 @@ console.log(5 ** 4 - 9);
 console.log("I".length - "Am".length + "Iron".length - "Man".length);
 
 // 4
-// ?????
+// huh???
+// kjsfgkjsgksj I GET IT NOW
+
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+// 5
+
+var m = 25;
+var c = 4;
+var u = m + c;
+console.log(u);
+
